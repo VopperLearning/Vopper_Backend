7de1b7dc454b1ad412041d8112d9cab9678827d0
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2024-09-13 23:30:07 -0500

    User_Service

---
+++ Modules/User_Module/User_service/ServiceImplementation/UserServiceImplement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using User_Module.User_service.ServiceInterface;
+using User_Module.User_core.Entities;
+using User_Module.User_core.Repository;
+
+namespace User_Module.User_service.ServiceImplementation
+{
+    public class UserServiceImplement : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserServiceImplement(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        //TODO: Implement the rest of the methods
+        public Task CreateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Modules/User_Module/User_service/ServiceInterface/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using User_Module.User_core.Entities;
+
+namespace User_Module.User_service.ServiceInterface
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(Guid id);
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(Guid id);
+
+    }
+}
