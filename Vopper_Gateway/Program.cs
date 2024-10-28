f5e709e785cbf859083d5f521abcb4560930c341
SantiAV25 <agredosantiago11@gmail.com>
2024-10-27 23:07:32 -0500

    First GateWayVersion

--- Vopper_Gateway/Program.cs
+++ Vopper_Gateway/Program.cs
@@ -1,9 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -14,7 +19,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseOcelot().Wait();
 
 var summaries = new[]
 {
