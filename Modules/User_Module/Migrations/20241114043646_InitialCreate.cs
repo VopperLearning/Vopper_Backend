eeddba818b011dc355927b0d3ecfe0249e1b3135
SantiAV25 <agredosantiago11@gmail.com>
2024-11-13 23:38:20 -0500

    Fixing the Problem With the database

---
+++ Modules/User_Module/Migrations/20241114043646_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace User_Module.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    User_Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    First_Name = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: true),
+                    Last_Name = table.Column<string>(type: "character varying(85)", maxLength: 85, nullable: true),
+                    Email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Password = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false),
+                    Verificate = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
