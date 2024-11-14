81ecf4a489214aa4f705b6a273352ae137d0b41e
SantiAV25 <agredosantiago11@gmail.com>
2024-11-13 23:08:31 -0500

    Fixing a table in User_Module

--- Modules/User_Module/User_core/Entities/User.cs
+++ Modules/User_Module/User_core/Entities/User.cs
@@ -4,7 +4,7 @@ namespace User_core.Entities
 {
     public class User
     {
-        public Guid Id { get; set; }
+        public long Id { get; set; }
         public string? User_Name { get; set; }
         public string? First_Name { get; set; }
         public string? Last_Name { get; set; }
